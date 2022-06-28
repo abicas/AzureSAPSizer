a980e9103be3543fc6899f80af0d8331adef0888
Alexandre Bicas <abicas@microsoft.com>
2022-06-28 17:17:43 -0300

    Adding comments on code

--- update_SAPSizer.py
+++ update_SAPSizer.py
@@ -5,60 +5,71 @@ from datetime import date
   
 # creating the date object of today's date
 todays_date = date.today()
+
+#setting up the filenames
 in_fname = "SAPs.xlsx"
 out_fname = "SAP Sizer BRA - "+str(todays_date.year)+str(todays_date.month)+str(todays_date.day)+".xlsx"
 
 http = urllib3.PoolManager()
 run = 1
-# url = "https://prices.azure.com/api/retail/prices?$filter=serviceName eq 'Virtual Machines' and endswith(productName,'Windows') eq false and meterName eq 'E32as v5' and ((currencyCode eq 'USD') or (currencyCode eq 'BRL')) and ((location eq 'Brazil South') or (location eq 'US East')) "
-alldata = []
 
+#opening SAPs.xlsx
 wb = load_workbook(in_fname)
 sheet = wb.worksheets[0]
 sheetprice = wb.worksheets[1]
 
 cell = sheet['A1']
-count=1
 print ('Reading from SAPs.xlsx columns :'+cell.value)
 
+##for each line with a certified VM name 
 while cell.value != None:
     count += 1
     cell = sheet['A'+str(count)]
+    ## if cell is not the last one 
     if cell.value != None:
         print ('Processing line: '+str(count)+' for VM: '+ cell.value)
         run = 1
+        ## build odata URL for pricelist with VM name
         url = "https://prices.azure.com/api/retail/prices?$filter=serviceName eq 'Virtual Machines' and endswith(productName,'Windows') eq false and meterName eq '"+cell.value+"' and ((currencyCode eq 'USD') or (currencyCode eq 'BRL')) and ((location eq 'BR South') or (location eq 'US East'))"
         while run: 
-            ##current url
-            # print (url)
+            ## get the json
             r = http.request('GET', url)
             data = json.loads(r.data)
+            ## for every item in the json query
             for data_item in data["Items"]: 
-                # print (data_item)
+                ## copy data from SAPs sheet
                 sheetprice['A'+str(count)].value = sheet['A'+str(count)].value
                 sheetprice['B'+str(count)].value = sheet['B'+str(count)].value
                 sheetprice['C'+str(count)].value = sheet['C'+str(count)].value
                 sheetprice['D'+str(count)].value = sheet['D'+str(count)].value
                 sheetprice['E'+str(count)].value = sheet['E'+str(count)].value
+                ## for now just USD is supported, making sure we are not getting additional currencies
                 if data_item["currencyCode"] == "USD":
-                    if "reservationTerm" in data_item: ##HAS RI
+                    ## If it is an RI Item
+                    if "reservationTerm" in data_item: 
+                        ## If this RI is for 3 Years 
                         if data_item["reservationTerm"] == "3 Years": ## is 3 Yr
+                            ## If in US East populate the column G  (RI3YR price in JSON is for 36 months, so dividing by 36 to get monthly prices)
                             if data_item["armRegionName"] == "eastus":
                                 sheetprice['G'+str(count)].value = data_item["retailPrice"]/36
                             else:
+                                ## Otherwise populate column I (RI3YR price in JSON is for 36 months, so dividing by 36 to get monthly prices)
                                 sheetprice['I'+str(count)].value = data_item["retailPrice"]/36
-                    else: ##PAYG
+                    else: ## Not RI, PAYG Then
+                        ## If in US, PAYG prices are hourly based in JSON
                         if data_item["armRegionName"] == "eastus":
                             sheetprice['F'+str(count)].value = data_item["retailPrice"]
                         else: 
+                            ## If not in US, PAYG prices are hourly based in JSON
                             sheetprice['H'+str(count)].value = data_item["retailPrice"]
-            #if paging is required, set url to nextpagelink
+            #if paging is required, there will be content for Next PageLink. set url to nextpagelink and loop
             if data['NextPageLink'] != None:
                 url = data['NextPageLink']
             else:
                 #otherwise finish loop 
                 run = 0
 
-
+## Save the file with DATE on the name
 wb.save(out_fname)
 print ("SAVED FILE ", out_fname)
+## Alright, go configure some SAP VMs ! 
